58c4a4dfc8b4e514996d0a40e57aa4636cadf74f
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2019-08-02 10:03:50 +0530

    Create area of circle cmn line.c

---
+++ area of circle cmn line.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[]) {
+    if (argc < 3)
+    {
+        printf(" Please provide values for both base and height \n");
+        return 0;
+        
+    } 
+    else
+    {
+
+    //float area,b,h;
+   // printf("enter the base and height");
+   // scanf("%f,%f",&b,&h);
+    int b=atoi(argv[1]);
+    int h=atoi(argv[2]);
+    float area=0.5*b*h;
+    printf("the are of given triangle is\t %0.2f",area);
+        
+    }
+    
+}
